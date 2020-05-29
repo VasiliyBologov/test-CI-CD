e94e38243e6006a3a77df192166f99c6487cefdb
Vaso <vasiliybologov@gmail.com>
2020-05-28 17:52:44 -0700

    last correct push to DockerHUB

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -5,6 +5,6 @@ if [ "$TRAVIS_BRANCH" = "master" ]; then
 else
     TAG="$TRAVIS_BRANCH"
 fi
-docker build -f Dockerfile -t testcicd:$TAG .
-docker tag testcicd:$TAG vasiliybologov/testcicd:$TAG
-docker push vasiliybologov/testcicd:$TAG
+docker build -f Dockerfile -t $DOCK_REPO_NAME:$TAG .
+docker tag $DOCK_REPO_NAME:$TAG $DOCKER_USER/$DOCK_REPO_NAME:$TAG
+docker push vasiliybologov/$DOCK_REPO_NAME:$TAG
