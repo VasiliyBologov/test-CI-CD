5a216631517efeac0636256c4539684872ac4ed0
Vaso <vasiliybologov@gmail.com>
2020-05-28 17:59:12 -0700

    lastest correct push to DockerHUB

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -7,4 +7,4 @@ else
 fi
 docker build -f Dockerfile -t $DOCK_REPO_NAME:$TAG .
 docker tag $DOCK_REPO_NAME:$TAG $DOCKER_USER/$DOCK_REPO_NAME:$TAG
-docker push vasiliybologov/$DOCK_REPO_NAME:$TAG
+docker push $DOCKER_USER/$DOCK_REPO_NAME:$TAG
